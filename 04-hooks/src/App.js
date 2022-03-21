25d42f6a5f5f5132722461abe0ab8142019c859c
Acevedo Ariel <AcevedoAriel@outlook.com>
2022-03-21 20:47:48 -0300

    input

--- 04-hooks/src/App.js
+++ 04-hooks/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Calculadora from "./components/Calculadora";
+import Effect from "./components/Effect";
 import State from "./components/State";
 
 const App = () => {
   return (
-    <State />
+    <div className="container text-center">
+      <Effect />
+    </div>
+
+    // <State />
     // <div className="container text-center d-inline">
     //   <h1>Calculadora - PWA</h1>
     //   <hr />
---
+++ 04-hooks/src/components/Effect.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Effect = () => {
+  return (
+    <>
+      <h1>useEffect</h1>
+      <hr />
+
+      <form>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default Effect;
