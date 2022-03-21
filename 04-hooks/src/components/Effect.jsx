d21cf7d1b17cad3f548a4787c3634336cb272353
Acevedo Ariel <AcevedoAriel@outlook.com>
2022-03-21 20:55:14 -0300

    update

--- 04-hooks/src/components/Effect.jsx
+++ 04-hooks/src/components/Effect.jsx
@@ -1,18 +1,29 @@
 import React from "react";
+// https://magic.reactjs.net/htmltojsx.htm
 
 const Effect = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log({
+      name: "kaksak",
+      email: "daddassd",
+      password: "sdasdad",
+    });
+  };
+
   return (
     <>
       <h1>useEffect</h1>
       <hr />
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="mb-3">
           <label htmlFor="exampleInputEmail1" className="form-label">
             Email address
           </label>
           <input
-            type="email"
+            type="text"
             className="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
