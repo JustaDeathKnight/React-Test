8b4041ced19537ecb452386e442e872aed871086
Gowther <AcevedoAriel@outlook.com>
2022-03-20 22:36:54 -0300

    set de control de cambio de estados

---
+++ 03-detalles/src/components/NumberInput.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const NumberInput = ({ name }) => {
+  const [numeros, setNumeros] = useState({
+    numero1: 0,
+    numero2: 0,
+  });
+
+  const handleChange = (e) => {
+    setNumeros({
+      numero1: parseFloat(e.target.value),
+      numero2: numeros.numero2,
+    });
+  };
+
+  const handleChange2 = (e) => {
+    setNumeros({
+      numero2: parseFloat(e.target.value),
+      numero1: numeros.numero1,
+    });
+  };
+
+  return (
+    <>
+      <label>
+        Numero 1:{" "}
+        <input value={numeros.numero1} onChange={handleChange} type="number" />
+      </label>
+      <label>
+        Numero 2:{" "}
+        <input value={numeros.numero2} onChange={handleChange2} type="number" />
+      </label>
+    </>
+  );
+};
+
+NumberInput.propTypes = {
+  name: PropTypes.string,
+};
+
+export default NumberInput;
