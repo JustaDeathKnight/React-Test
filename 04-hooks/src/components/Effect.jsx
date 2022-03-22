0375070ee54113cb8a99c1884325fc601b2f309f
Acevedo Ariel <AcevedoAriel@outlook.com>
2022-03-22 12:42:17 -0300

    prueba de useEffect

--- 04-hooks/src/components/Effect.jsx
+++ 04-hooks/src/components/Effect.jsx
@@ -1,15 +1,33 @@
 import React from "react";
+import { useState, useEffect } from "react";
 // https://magic.reactjs.net/htmltojsx.htm
 
 const Effect = () => {
+  // const [users, setUsers] = useState([]);
+  // const [id, setId] = useState(10);
+
+  // useEffect(() => {
+  //   // https://jsonplaceholder.typicode.com/
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setUsers(data);
+  //     });
+  // }, []);
+
+  // useEffect(() => {
+  //   setUsers({});
+  //   console.log(users);
+  // }, [id]);
+
+  const [first, setfirst] = useState(0);
+
+  useEffect(() => {
+    window.addEventListener("mousemove", (e) => {});
+  }, []);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    console.log({
-      name: "kaksak",
-      email: "daddassd",
-      password: "sdasdad",
-    });
   };
 
   return (
